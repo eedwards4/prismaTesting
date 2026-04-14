4deee3fe3d7d1bd22945fdd9bfc81655d7020690
Ethan Edwards <ethan.edwards@fusiontechnologysolutions.com>
2026-04-14 12:16:52 -0700

    Started work on framework for easy hotswapping of test platforms (e.g AHK, Selenium, etc)

--- prisma_tester.py
+++ prisma_tester.py
@@ -21,6 +21,20 @@ def urlStripper(line):
     return url
 
 
+# Test programs
+def asAHK(url):
+    # Run AHK script on URL
+    subprocess.run([AHK_PATH, SCRIPT, url])
+    
+    file = open("{}/AppData/Local/Temp/ahk_output.txt".format(Path.home()))
+    content = file.readline().strip()
+    file.close()
+
+    if not "TRUE" in content:
+        return True
+    
+    return False
+
 
 def run_test(filepath):
     num_unblocked = 0
@@ -42,14 +56,7 @@ def run_test(filepath):
             url = urlStripper(line)
 
             if url != "VOID":
-                # Run AHK script on URL
-                subprocess.run([AHK_PATH, SCRIPT, url])
-    
-                file = open("{}/AppData/Local/Temp/ahk_output.txt".format(Path.home()))
-                content = file.readline().strip()
-                file.close()
-    
-                if not "TRUE" in content:
+                if asAHK(url):
                     num_unblocked += 1
                     unblocked_list.append(url)
                 
