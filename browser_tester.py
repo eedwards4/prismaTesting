7e982de3c6c18c2049baf54dd8568ec4823d6921
Ethan Edwards (VIRSBX-HEAD UNSAFE) <ethan.edwards@fusiontechnologysolutions.com>
2026-05-04 14:44:46 -0700

    Potential fix

--- browser_tester.py
+++ browser_tester.py
@@ -123,7 +123,10 @@ def asWEBDRVR(url):
         driver.get(url)
     except Exception as e:
         driver.execute_script("window.stop();")
-        if VERBOSE: print("WARN: Caught Exception {} || {} || Elapsed: {}".format(e, url, datetime.timedelta(seconds=(time.perf_counter() - start))))
+        if ("net::ERR_NAME_NOT_RESOLVED" in e):
+            if VERBOSE: print("Ignored Name Resolution Error || {} || Elapsed: {}".format(url, datetime.timedelta(seconds=(time.perf_counter() - start))))
+        else:
+            if VERBOSE: print("{} || WARN: Encountered the following error: \n {}".format(url, e))
         return False
     
     WebDriverWait(driver, 1).until(
